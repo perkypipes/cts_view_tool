3a0b19fe00f8330332fedc85b16d118386086c34
Jordan Hudgens <jordan@devcamp.com>
2017-07-06 15:24:57 +0000

    dev tool ititial commit

---
+++ lib/cts_view_tool.rb
@@ -0,0 +1,6 @@
+require "cts_view_tool/version"
+require "cts_view_tool/renderer"
+
+module CtsViewTool
+  # Your code goes here...
+end
---
+++ lib/cts_view_tool/version.rb
@@ -0,0 +1,3 @@
+module CtsViewTool
+  VERSION = "0.1.0"
+end
